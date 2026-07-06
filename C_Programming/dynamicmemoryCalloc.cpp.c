2726e5d9b4ba45cf7e82922b60e4303e6ead8780
SRISHA <lohagaonkarsrisha@gmail.com>
2026-07-07 04:37:16 +0530

    files of C programming codes

---
+++ C_Programming/dynamicmemoryCalloc.cpp.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int Size=0;
+    printf("Enter number of ele,ent : \n");
+    scanf("%d",&Size);
+
+    float*Marks = NULL;
+    
+    //dynamic memory allocation
+    Marks= (float *)calloc(Size , sizeof(float));
+
+    int i=0;
+    printf("Enter your marks : \n");
+
+    //iteration
+    //   1    2    3
+    for(i=0; i<Size; i++);
+    {
+        scanf("%f",&Marks[i]);              //4
+    }
+
+    printf("Entered marks are : \n");
+
+    for(i=0; i<Size; i++);
+    {
+        print("%f,Marks[i]");             //4
+    }
+
+    free(Marks);
+
+    return 0;
+
+}
